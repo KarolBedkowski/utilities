042549d3e21fdd5476a17f8fcb019555512dc9f9
Karol Będkowski <karol.bedkowski@gmail.com>
2014-10-03 22:21:20 +0200

    v0.2

--- jamendo_mp3.py
+++ jamendo_mp3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
+""" Fix tags in files from Jamendo; rename files. """
 
 import os
 import re
 import unicodedata
 
-from mutagen.easyid3 import EasyID3
-from mutagen.mp3 import MP3
+import mutagen
 
 
 _RE_REM_TITLE = re.compile(r'^(.+) - \d\d - (.+)$')
@@ -30,57 +30,59 @@ def filename_from_tags(tags):
 
 def process_file(filename, idx, len_files):
     print 'Processing ', filename
-    if filename.lower().endswith('.mp3'):
-        mediafile = MP3(filename, ID3=EasyID3)
-    else:
-        raise NotImplementedError('unknown file type: ' + filename)
-    #_print_tags(mediafile.tags)
+    tags = mutagen.File(filename, easy=True)
+    _print_tags(tags)
     # fix performer
-    if not mediafile.tags.get('performer'):
-        if mediafile.tags.get('artist'):
-            mediafile.tags['performer'] = [mediafile.tags['artist'][0]]
+    if not tags.get('performer'):
+        if tags.get('artist'):
+            tags['performer'] = [tags['artist'][0]]
     # fix title
-    artist = mediafile.tags.get('artist')
+    artist = tags.get('artist')
     title = os.path.splitext(os.path.basename(filename))[0]
-    if mediafile.tags.get('title'):
-        title = mediafile.tags['title'][0]
+    if tags.get('title'):
+        title = tags['title'][0]
     if title:
         mtitle = _RE_REM_TITLE.match(title)
         if mtitle:
             title = mtitle.group(2)
-            mediafile.tags['artist'] = mtitle.group(1)
+            tags['artist'] = mtitle.group(1)
     if title and artist and title.startswith(artist[0] + " - "):
         title = title[len(artist[0]) + 3:]
     if title:
-        mediafile.tags['title'] = [title]
+        tags['title'] = [title]
     else:
         print '[E]: missing title'
     # fix year
-    if not mediafile.tags.get('date'):
-        copyr = mediafile.tags.get('copyright')
+    if not tags.get('date'):
+        copyr = tags.get('copyright')
         if copyr and copyr[0]:
             year_re = _RE_GET_DATE_FROM_CR.match(copyr[0])
             if year_re and year_re.group(1):
-                mediafile.tags['date'] = [str(year_re.group(1))]
+                tags['date'] = [str(year_re.group(1))]
     # fix tracknumber
     if len_files == 1:
         # remove track number when only one file
-        if 'tracknumber' in mediafile.tags:
-            del mediafile.tags['tracknumber']
+        if 'tracknumber' in tags:
+            del tags['tracknumber']
     else:
-        curr_tracknum = mediafile.tags.get('tracknumber')
+        curr_tracknum = tags.get('tracknumber')
         if curr_tracknum and curr_tracknum[0]:
             if not _RE_CHECK_TRACK_NUM.match(curr_tracknum[0]):
-                mediafile.tags['tracknumber'] = [curr_tracknum[0] +
-                                                 '/' +
-                                                 str(len_files)]
+                curr_tracknum = curr_tracknum[0]
+                try:
+                    curr_tracknum = "%02d" % int(curr_tracknum)
+                except ValueError:
+                    pass
+                tags['tracknumber'] = [curr_tracknum +
+                                       '/' +
+                                       str(len_files)]
         else:
-            mediafile.tags['tracknumber'] = [str(idx) + '/' + str(len_files)]
+            tags['tracknumber'] = [("%02d" % idx) + '/' + str(len_files)]
     #print 'Result: '
-    #_print_tags(mediafile.tags)
-    mediafile.save()
+    _print_tags(tags)
+    tags.save()
     print 'Saved\n'
-    return mediafile.tags
+    return tags
 
 
 def main(dirname='.'):
