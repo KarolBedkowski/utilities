84f3b5d54a97b0721eb90538dc3cfbd9e29a3a29
Karol Będkowski <karol.bedkowski@gmail.com>
2016-07-10 12:58:25 +0200

    todo: + recurse tasks

--- todotxt_util.py
+++ todotxt_util.py
@@ -27,12 +27,17 @@ import argparse
 import re
 import locale
 import time
+import datetime
+
+from dateutil.relativedelta import relativedelta
 
 _MATCH_CONTEXT_RE = re.compile(r'(@\S+)', re.L)
 _MATCH_PROJECT_RE = re.compile(r'\s(\+\S+)', re.L)
 _MATCH_PRIORITY_RE = re.compile(r'\(([A-Za-z])\)', re.L)
-_MATCH_DUE_RE = re.compile(r'due:(\d\d\d\d-\d\d-\d\d)')
-_MATCH_TDUE_RE = re.compile(r't:(\d\d\d\d-\d\d-\d\d)')
+_MATCH_DUE_RE = re.compile(r' due:(\d\d\d\d-\d\d-\d\d)')
+_MATCH_TDUE_RE = re.compile(r' t:(\d\d\d\d-\d\d-\d\d)')
+_MATCH_R_RE = re.compile(r' rec:(\+?\d+)([dmyw]?)', re.I)
+_MATCH_STATUS_RE = re.compile(r'^(x?)( (\d\d\d\d-\d\d-\d\d))? ', re.I)
 
 
 def _today():
@@ -54,7 +59,11 @@ def _load_task(line):
             'project': "\xff",
             'context': "\xff",
             'priority': "\xff",
-            'status': 'a'}
+            'status': 'a',
+            'status_date': None,
+            'recurse': None,
+            'due': None,
+            'tdue': None}
     context = _MATCH_CONTEXT_RE.search(line)
     if context:
         task['context'] = context.group(1)
@@ -80,8 +89,14 @@ def _load_task(line):
             task['over_t'] = date - _NOW
         except ValueError:
             pass
-    if line.startswith('x ') or line.startswith('X '):
-        task['status'] = "x"
+    status = _MATCH_STATUS_RE.search(line)
+    if status:
+        task['status'] = status.group(1).lower()
+        if len(status.groups()) == 3:
+            task['status_date'] = status.group(3)
+    rec = _MATCH_R_RE.search(line)
+    if rec:
+        task['recurse'] = (rec.group(1), rec.group(2))
     #print(task)
     return task
 
@@ -141,7 +156,12 @@ def archive_tasks(args):
     """Move done tasks to archive"""
     if args.verbose:
         print("loading {} file".format(args.file), file=sys.stderr)
-    tasks = list(load_tasks(args.file))
+    tasks = load_tasks(args.file)
+
+    if args.recurse:
+        tasks = recurse_tasks(tasks)
+
+    tasks = list(tasks)
 
     done = (task for task in tasks if task['status'] == 'x')
     active = (task for task in tasks if task['status'] != 'x')
@@ -163,6 +183,68 @@ def archive_tasks(args):
         print("done", file=sys.stderr)
 
 
+def _move_date(indate, offset):
+    if indate and offset:
+        data = datetime.date.fromtimestamp(indate)
+        data += offset
+        return time.mktime(data.timetuple())
+    return indate
+
+
+def _replace_date(content, prefix, old_date, new_date):
+    if not old_date or not new_date:
+        return content
+    replstr = prefix + time.strftime("%Y-%m-%d", time.localtime(old_date))
+    deststr = prefix + time.strftime("%Y-%m-%d", time.localtime(new_date))
+    return content.replace(replstr, deststr)
+
+
+def recurse_tasks(tasks):
+    """create new task from finished tasks with 'r' tag """
+    for task in tasks:
+        yield task
+        rec = task['recurse']
+        if task['status'] != 'x' or not rec:
+            continue
+        oval = 0
+        try:
+            oval = int(rec[0])
+        except ValueError as err:
+            print("error {}".format(err), file=sys.stderr)
+            continue
+        offset = None
+        if rec[1] == 'm':
+            offset = relativedelta(months=oval)
+        elif rec[1] == 'w':
+            offset = relativedelta(weeks=oval)
+        elif rec[1] == 'y':
+            offset = relativedelta(years=oval)
+        elif rec[1] == 'q':
+            offset = relativedelta(months=oval*3)
+        elif rec[1] == 'd':
+            offset = relativedelta(days=oval*3)
+        else:
+            print("error: unknown rec step: {}", rec, file=sys.stderr)
+            continue
+
+        if args.verbose:
+            print("recurse task '{}'".format(task['content']),
+                  file=sys.stderr)
+
+        ntask = task.copy()
+        content = ntask['content'][2:]
+        if task['status_date']:
+            content  = content[11:]
+        ntask['status'] = 'a'
+        ntask['tdue'] = _move_date(task['tdue'], offset)
+        content = _replace_date(content, ' t:', task['tdue'], ntask['tdue'])
+        ntask['due'] = _move_date(task['due'], offset)
+        content = _replace_date(content, ' due:', task['due'], ntask['due'])
+        ntask['content'] = content
+        #print("REC: {}".format(ntask))
+        yield ntask
+
+
 _SORT_KEYS_SK = {
     'p': 'project',
     'd': 'over_due',
@@ -178,6 +260,9 @@ def sort_tasks(args):
     if args.verbose:
         print("loading {} file".format(args.file), file=sys.stderr)
     tasks = load_tasks(args.file)
+
+    if args.recurse:
+        tasks = recurse_tasks(tasks)
 
     mode = args.mode or _DEFAULT_SORT_MODE
     s_keys = [_SORT_KEYS_SK[char] for char in mode if char in _SORT_KEYS_SK]
@@ -220,7 +305,13 @@ def parse_args():
     parser_sort.add_argument(
         '--archive', action="store_true",
         help='archive done tasks')
-    subparsers.add_parser('archive', help='archive done tasks')
+    parser_sort.add_argument(
+        '--recurse', action="store_true",
+        help='create new task according to recurse tags')
+    parser_arch = subparsers.add_parser('archive', help='archive done tasks')
+    parser_arch.add_argument(
+        '--recurse', action="store_true",
+        help='create new task according to recurse tags')
     return parser.parse_args()
 
 
@@ -237,6 +328,7 @@ def main():
     if args.command == 'sort':
         sort_tasks(args)
         if args.archive:
+            args.recurse = False
             archive_tasks(args)
 
     if args.command == 'archive':
